9e2343590b9ca3c8223746ea37f244458bea2194
GreenPixel10 <evan@raz-family.ca>
2024-04-05 19:28:58 -0400

    clean up

--- Python/main.py
+++ Python/main.py
@@ -1,13 +1,8 @@
 from input_helper import *
 
-from credentials import *
-
-from user_defs import *
+from sql_helper import *
 
 
-
-user = user_class(0)
-          
 
 def __main__():
 
@@ -16,9 +11,8 @@ def __main__():
      choice = get_menu_input("What would you like to do?", ["Login", "Register"])
 
      match choice:
-          case 0: user = log_in()
-          case 1: user = register()
+          case 0: pass
+          case 1: pass
 
-     user.show_options(user)
 
 __main__()
